d910cebb42ad4ad8906174b75668bb7126be2aa9
MrHedgehogBY <bylkovevgen00@gmail.com>
2020-11-12 22:43:38 +0300

    First commit

--- LabProject/Startup.cs
+++ LabProject/Startup.cs
@@ -16,6 +16,7 @@ namespace LabProject
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,9 @@ namespace LabProject
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+                app.UseStaticFiles();
+                app.UseMvcWithDefaultRoute();
             }
 
             app.UseRouting();
