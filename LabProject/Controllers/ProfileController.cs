40e0dae4c6183b053bc4f5c9c7835f9189caf689
MrHedgehogBY <bylkovevgen00@gmail.com>
2020-12-02 20:15:33 +0300

    working with views

---
+++ LabProject/Controllers/ProfileController.cs
@@ -0,0 +1,40 @@
+﻿using LabProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabProject.Controllers
+{
+    public class ProfileController: Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public ProfileController(UserManager<User> userManager, SignInManager<User> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Profile(string name)
+        {
+            User user = await _userManager.FindByNameAsync(name);
+            ViewBag.Id = user.Id;
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
